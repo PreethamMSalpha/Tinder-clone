a1086a90eef9393aae747b1ed90c9bb9e15dcb21
Preetham.M.S <preethamms619@gmail.com>
2020-03-01 20:38:36 +0530

    added name,gender to registation n connected to DB

--- app/src/main/java/com/pklabs/tinder/RegistrationActivity.java
+++ app/src/main/java/com/pklabs/tinder/RegistrationActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,11 +17,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button mRegister;
-    private EditText mEmail,mPassword;
+    private EditText mEmail,mPassword,mName;
+
+    private RadioGroup mRadioGroup;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
@@ -48,19 +57,42 @@ public class RegistrationActivity extends AppCompatActivity {
         mRegister = (Button)findViewById(R.id.register);
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
+        mName = (EditText) findViewById(R.id.name);
+
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
 
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                int selected = mRadioGroup.getCheckedRadioButtonId();
+                final RadioButton radioButton = (RadioButton)findViewById(selected);
+
+                if (radioButton.getText() == null){
+                    return;
+                }
+
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
+                final String name = mName.getText().toString();
                 mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if (!task.isComplete()){
+
                             Toast.makeText(RegistrationActivity.this, "registration unsuccessful", Toast.LENGTH_SHORT).show();
+
+                        }else{
+
+                            String userId = mAuth.getCurrentUser().getUid();
+                            //DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(radioButton.getText().toString()).child(userId).child("name");
+                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(radioButton.getText().toString()).child(userId).child("name");
+
+                            currentUserDb.setValue(name);
+
+
                         }
 
                     }
