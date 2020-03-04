8fed09262bce5d06ef9e3e35e53c7570ba55028c
Preetham.M.S <preethamms619@gmail.com>
2020-03-04 18:09:26 +0530

    Making changes to arrayAdapter,cards and MainActivity to display ProfileImages on the Swipecards

--- app/src/main/java/com/pklabs/tinder/MainActivity.java
+++ app/src/main/java/com/pklabs/tinder/MainActivity.java
@@ -241,7 +241,7 @@ public class MainActivity extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 if (dataSnapshot.exists() && !dataSnapshot.child("connections").child("nope").hasChild(currentUId) && !dataSnapshot.child("connections").child("yup").hasChild(currentUId)){
                     //al.add(dataSnapshot.child("name").getValue().toString());
-                    cards item = new cards(dataSnapshot.getKey(),dataSnapshot.child("name").getValue().toString());
+                    cards item = new cards(dataSnapshot.getKey(), dataSnapshot.child("name").getValue().toString(), dataSnapshot.child("profileImageUrl").getValue().toString());
                     rowItems.add(item);
                     arrayAdapter.notifyDataSetChanged();
                 }
