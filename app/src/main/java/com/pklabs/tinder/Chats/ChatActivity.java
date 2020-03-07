fa1a8287b44c01267001d2183de5d186282017f9
Preetham.M.S <preethamms619@gmail.com>
2020-03-08 00:00:20 +0530

    Sending messages and storing in Firebase

--- app/src/main/java/com/pklabs/tinder/Chats/ChatActivity.java
+++ app/src/main/java/com/pklabs/tinder/Chats/ChatActivity.java
@@ -1,19 +1,30 @@
 package com.pklabs.tinder.Chats;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pklabs.tinder.Matches.MatchesActivity;
 import com.pklabs.tinder.Matches.MatchesAdapter;
 import com.pklabs.tinder.Matches.MatchesObject;
 import com.pklabs.tinder.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -21,15 +32,26 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView.Adapter mChatAdapter;
     private RecyclerView.LayoutManager mChatLayoutManager;
 
-    private  String currentUserId;
+    private  String currentUserId, matchId, chatId;
+
+    private EditText mSendEditText;
+    private Button mSendButton;
+
+    DatabaseReference mDatabaseUser, mDatabaseChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        matchId = getIntent().getExtras().get("matchId").toString();
+
         currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId).child("connections").child("matches").child(matchId).child("chatId");
+        mDatabaseChat = FirebaseDatabase.getInstance().getReference().child("Chat");
+
+        getChatId();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
@@ -38,7 +60,51 @@ public class ChatActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mChatLayoutManager);
         mChatAdapter = new ChatAdapter(getDataSetChat(), ChatActivity.this);
         mRecyclerView.setAdapter(mChatAdapter);
+
+        mSendEditText = (EditText) findViewById(R.id.message);
+        mSendButton = (Button) findViewById(R.id.send);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
     }
+
+    private void sendMessage() {
+        String sendMessageText = mSendEditText.getText().toString();
+
+        if (!sendMessageText.isEmpty()){
+            DatabaseReference  newMessageDb = mDatabaseChat.push();
+
+            Map newMessage = new HashMap();
+            newMessage.put("createdByUser", currentUserId);
+            newMessage.put("text", sendMessageText);
+
+            newMessageDb.setValue(newMessage);
+        }
+        mSendEditText.setText(null);
+    }
+
+    private void getChatId(){
+        mDatabaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    chatId = dataSnapshot.getValue().toString();
+                    mDatabaseChat = mDatabaseChat.child(chatId);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     private ArrayList<ChatObject> resultsChat= new ArrayList<ChatObject>();
     private List<ChatObject> getDataSetChat() {
         return  resultsChat;
